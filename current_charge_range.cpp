e43b9c00a99b7077661ac52c5201cf6b60822c3c
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-01-27 18:47:35 +0530

    Update current_charge_range.cpp

--- current_charge_range.cpp
+++ current_charge_range.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 extern int input_current_charge[] = { 3, 3, 5, 4, 10, 11, 12 };
 
-string detect_range(int low_range, int upp_range)
+void detect_range(int low_range, int upp_range)
 {
 	int range_count = 0;
 
