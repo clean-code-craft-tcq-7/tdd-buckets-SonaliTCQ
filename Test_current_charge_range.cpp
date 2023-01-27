dd3ef8e43e64072ee9bb31b554bfcdd78b679459
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-01-27 19:01:43 +0530

    Update Test_current_charge_range.cpp

--- Test_current_charge_range.cpp
+++ Test_current_charge_range.cpp
@@ -1,1 +1,17 @@
+#include <iostream.h>
+#include "current_charge_range.hpp"
 
+TEST_CASE("Show range for current samples")
+{
+  
+  std::ostringstream oss;
+  std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+  std::cout.rdbuf(oss.rdbuf());
+
+  detect_range(3,5);
+
+  std::cout.rdbuf(p_cout_streambuf);
+  
+  REQUIRE(oss.str() == "3-5, 4\n";
+          
+}
