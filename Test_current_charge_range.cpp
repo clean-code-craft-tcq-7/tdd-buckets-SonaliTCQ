a83a80e71f064955cbbda9fa57a7b3814533373a
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2023-01-27 19:07:57 +0530

    Update Test_current_charge_range.cpp

--- Test_current_charge_range.cpp
+++ Test_current_charge_range.cpp
@@ -1,3 +1,5 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
 #include "current_charge_range.hpp"
 #include "test/catch.hpp"
 #include <iostream>
